46708d48db380e226cde062973dc6831a25d2aa0
Mebayonas Yonas <mebayonasyonas@gmail.com>
2024-12-08 20:48:48 +0300

    final commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
 	// baseURL: "http://127.0.0.1:5001/miko-clon/us-central1/api",
-    baseURL:"https://amazon-api-deploy-qttz.onrender.com"
+    baseURL:"https://amazon-clone-api-wl1t.onrender.com"
 });
 
 export { axiosInstance };
